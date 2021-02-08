538dce1afabf34a8997012de6f9d28b1c6f1b77f
Sagramielgo <sagramielgo@gmail.com>
2021-02-08 16:29:26 +0100

    planning exercise flowchart

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('>> Ready :)');
+/* DIAGRAMA 
+1. Arrancar la página.
+   1.1 Coger datos del api con un Fetch
+   1.2 EVENTO: Excuchar botón de búsqueda con el contenido del value.
+   1.3 PINTAR las series
+
+2. FILTRAR: recoger el valor del input y filtrar las series
+   3.1 PINTAR series
+   3.2 ESCUCHAR eventos en las series (al clickar en ellas que cambien de color, o algún cambio)
+ 
+ 
+ 3. EVENTO: clicar en una de las series y:
+   2.1 Marcar o desmarcar dicha serie de una sección de "favoritas" en los datos
+   2.3 PINTAR de nuevo las series
+
+   4. Meter en un array las favoritas
+   4.1 Pintar el array de favoritas en la izq de la pantalla (section, div, aside?)
+   4.2 Guardar favoritas en localSorage
+
+   BONUS:
+    5. Escuchar botón borrar cada serie de favoritos
+    5.1 botón borrar todos los favoritos
+*/
