715fad8cab21c919cd9df05ded9dc1cc21de932e
Sagramielgo <sagramielgo@gmail.com>
2021-02-08 19:00:15 +0100

    clean and listen event for handleInputSearch function

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,8 @@
 /* DIAGRAMA 
 1. Arrancar la página. OK
    1.1 Coger datos del api con un Fetch OK
-   1.2 EVENTO: Excuchar botón de búsqueda con el contenido del value.
-   1.3 PINTAR las series
+   1.2 EVENTO: Excuchar botón de búsqueda con el contenido del value.OK
+   1.3 PINTAR las series OK
 
 2. FILTRAR: recoger el valor del input y filtrar las series
    3.1 PINTAR series
@@ -23,13 +23,19 @@
     5. Escuchar botón borrar cada serie de favoritos
     5.1 botón borrar todos los favoritos
 */
+const formElement = document.querySelector('.js-form');
+const searchBtnElement = document.querySelector('.js-searchButton');
+
+const showsContainerElement = document.querySelector('.js-showsContainer');
+const inputElement = document.querySelector('.js-input');
+const inputValue = inputElement.value;
 
 // variable de los datos que me devuelve el api
 let series = [];
-const inputElement = document.querySelector('.js-input');
-const inputValue = inputElement.value;
+
 //CREAR función y meter api dentro
 function getDataFromApi() {
+  const inputValue = inputElement.value;
   fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     //for para saltarnos el 'score' y que data nos devuelva directamente 'show'
@@ -38,16 +44,22 @@ function getDataFromApi() {
         const showList = data[index].show;
         series.push(showList);
       }
-      console.log(series);
       paintSeries();
     });
 }
 
-//pintar nombre
-const showsContainerElement = document.querySelector('.js-showsContainer');
+// evitar que envíe al dar intro al input
+function handleForm(ev) {
+  ev.preventDefault();
+}
+formElement.addEventListener('submit', handleForm);
 
+//imagen por defecto
+const placeholderImg =
+  'https://via.placeholder.com/210x295/add8e6/000ff0/?text=';
+
+//PINTAR en HTML
 function paintSeries() {
-  console.log('Estoy pintando esto:', series);
   let codeHTML = '';
   for (let index = 0; index < series.length; index++) {
     const name = series[index].name;
@@ -65,10 +77,11 @@ function paintSeries() {
   }
   showsContainerElement.innerHTML = codeHTML;
 }
-
-const placeholderImg =
-  'https://via.placeholder.com/210x295/add8e6/000ff0/?text=';
-
 paintSeries();
-//start api
-getDataFromApi();
+
+function handleInputSearch() {
+  getDataFromApi();
+  console.log('me has clicado', showsContainerElement);
+  paintSeries();
+}
+searchBtnElement.addEventListener('click', handleInputSearch);
